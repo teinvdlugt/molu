00502d5e7c20e3c1429f94e0809aeb9c7744725c
Tein van der Lugt <teinvdlugt@gmail.com>
2014-10-18 15:14:39 +0200

    Add the abbreviations to the list with elements

--- app/src/main/java/com/teinproductions/tein/molu/ElementListAdapter.java
+++ app/src/main/java/com/teinproductions/tein/molu/ElementListAdapter.java
@@ -26,10 +26,12 @@ public class ElementListAdapter extends ArrayAdapter {
         TextView elementNameTextView = (TextView) theView.findViewById(R.id.element_name_text_view);
         TextView atomicNumberTextView = (TextView) theView.findViewById(R.id.atomic_number_text_view);
         TextView atomicMassTextView = (TextView) theView.findViewById(R.id.atomic_mass_text_view);
+        TextView abbreviationTextView = (TextView) theView.findViewById(R.id.abbreviation_text_view);
 
         elementNameTextView.setText(Element.values()[position].getName());
         atomicNumberTextView.setText(Element.values()[position].getAtomicNumber().toString());
         atomicMassTextView.setText(Element.values()[position].getMass().toString());
+        abbreviationTextView.setText(Element.values()[position].getAbbreviation());
 
         return theView;
     }
