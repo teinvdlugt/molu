45c38cb392c72e3b3ccf14501e9b4ca7952ce697
Tein van der Lugt <teinvdlugt@gmail.com>
2014-11-06 20:32:12 +0100

    Delete activity_element_info.xml and create interface in ElementListFragment

--- app/src/main/java/com/teinproductions/tein/molu/ElementListFragment.java
+++ app/src/main/java/com/teinproductions/tein/molu/ElementListFragment.java
@@ -1,10 +1,12 @@
 package com.teinproductions.tein.molu;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 
 public class ElementListFragment extends ListFragment {
 
@@ -19,5 +21,23 @@ public class ElementListFragment extends ListFragment {
         setListAdapter(new ElementListAdapter(getActivity(), elementNames));
 
         return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    public interface onElementClickListener{
+        public void onElementClick();
+    }
+
+    onElementClickListener mCallBack;
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+
+        try {
+            mCallBack = (onElementClickListener) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString() + " must implement ElementListFragment.onElementClickListener");
+        }
+
     }
 }
