299d9c303d78d93b5924428d5ae6cf8f22a6ff71
Tein van der Lugt <teinvdlugt@gmail.com>
2014-11-06 20:24:42 +0100

    Fill ElementInfoActivity with ElementListFragment
    Not using activity_element_info.xml any more

--- app/src/main/java/com/teinproductions/tein/molu/ElementInfoActivity.java
+++ app/src/main/java/com/teinproductions/tein/molu/ElementInfoActivity.java
@@ -10,34 +10,23 @@ import android.widget.ListView;
 
 public class ElementInfoActivity extends ActionBarActivity{
 
-    ListView listView;
-
     public static String ELEMENTRESULTINT = "Index of the element that must be sent back to MainActivity";
 
+    ElementListFragment elementListFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        this.setContentView(R.layout.activity_element_info);
 
-        // TODO getActionBar().setHomeButtonEnabled(true);
+        elementListFragment = new ElementListFragment();
 
-        listView = (ListView) findViewById(R.id.element_info_list_view);
-
-        String[] elementNames = new String[Element.values().length];
-        for(int i = 0; i < elementNames.length; i++){
-            elementNames[i] = Element.values()[i].getName();
+        if(savedInstanceState == null){
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(android.R.id.content, elementListFragment)
+                    .commit();
         }
 
-        ElementListAdapter adapter = new ElementListAdapter(this, elementNames);
-
-        listView.setAdapter(adapter);
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                finishWithResult(position);
-            }
-        });
     }
 
     private void finishWithResult(int position) {
