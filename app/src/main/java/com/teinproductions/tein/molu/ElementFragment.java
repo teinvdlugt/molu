38e8ef8b2c36a4fdf1f69d6e117f01bf59c524c1
Tein van der Lugt <teinvdlugt@gmail.com>
2014-11-07 17:33:57 +0100

    Change some layout

--- app/src/main/java/com/teinproductions/tein/molu/ElementFragment.java
+++ app/src/main/java/com/teinproductions/tein/molu/ElementFragment.java
@@ -12,7 +12,7 @@ import java.text.DecimalFormat;
 
 public class ElementFragment extends Fragment{
 
-    TextView elementNameTextView, atomicMassTextView, abbreviationTextView, atomicNumberTextView;
+    TextView atomicMassTextView, abbreviationTextView, atomicNumberTextView;
 
     @Nullable
     @Override
@@ -20,7 +20,6 @@ public class ElementFragment extends Fragment{
 
         View theView = inflater.inflate(R.layout.fragment_element, container, false);
 
-        elementNameTextView = (TextView) theView.findViewById(R.id.fragment_element_name);
         atomicMassTextView = (TextView) theView.findViewById(R.id.fragment_element_atomic_mass);
         abbreviationTextView = (TextView) theView.findViewById(R.id.fragment_element_abbreviation);
         atomicNumberTextView = (TextView) theView.findViewById(R.id.fragment_element_atomic_number);
@@ -31,7 +30,6 @@ public class ElementFragment extends Fragment{
 
         Element element = (Element) getArguments().getSerializable("element");
 
-        elementNameTextView.setText(element.getName());
         atomicMassTextView.setText(new DecimalFormat().format(element.getMass()));
         abbreviationTextView.setText(element.getAbbreviation());
         atomicNumberTextView.setText(new DecimalFormat().format(element.getAtomicNumber()));
