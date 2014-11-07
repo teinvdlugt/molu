f1edc9318b21cec25f57f0ade15b4154726b25fe
Tein van der Lugt <teinvdlugt@gmail.com>
2014-11-07 14:09:52 +0100

    Don't show list menu item when current page is list

--- app/src/main/java/com/teinproductions/tein/molu/ElementPagerActivity.java
+++ app/src/main/java/com/teinproductions/tein/molu/ElementPagerActivity.java
@@ -5,6 +5,8 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class ElementPagerActivity extends ActionBarActivity
                     implements ElementListFragment.onElementClickListener,
@@ -33,8 +35,45 @@ public class ElementPagerActivity extends ActionBarActivity
             }
         });
 
+        theViewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int i, float v, int i2) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                invalidateOptionsMenu();
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int i) {
+
+            }
+        });
+
         setContentView(theViewPager);
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.element_pager, menu);
+        menu.findItem(R.id.pager_activity_show_list_action).setVisible(!(theViewPager.getCurrentItem() == 0));
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.pager_activity_show_list_action){
+            theViewPager.setCurrentItem(0, true);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
